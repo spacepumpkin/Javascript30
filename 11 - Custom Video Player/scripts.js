1116ebc917e23424f8cb81f5f0a4f808a7a2e5f0
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2021-04-22 23:42:48 -0700

    Project 11 (Custom Video Player) - Add progress bar and fullscreen features

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,12 +2,14 @@
 const video = document.querySelector("video.player__video");
 const playButton = document.querySelector("button[title='Toggle Play']");
 const skipButtons = document.querySelectorAll("button[data-skip]");
-const volumeSlider = document.querySelector("input[name='volume']");
-const playrateSlider = document.querySelector("input[name='playbackRate']");
+const sliders = document.querySelectorAll("input[type='range']");
+const progress = document.getElementsByClassName("progress")[0];
+const progressBar = document.getElementsByClassName("progress__filled")[0];
+const fullscreenButton = document.querySelector("button.fullscreen");
 
 // Make functions
 function togglePlay() {
-  video.paused ? video.play() : video.pause(); 
+  video.paused ? video.play() : video.pause();
 }
 
 function updatePlayButton() {
@@ -19,28 +21,47 @@ function skip() {
   video.currentTime += parseFloat(skipSeconds);
 }
 
-// function adjustVolume(evt) {
-//   if (evt.buttons < 1) return;
-//   console.log(evt);
-//   video.volume = this.value;
-// }
-
 function handleRangeUpdate(evt) {
   if (evt.buttons < 1) return;
   console.log(evt);
   video[this.name] = this.value;
 }
 
+function updateProgressBar() {
+  const videoProgress = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${videoProgress}%`;
+}
+
+function scrub(evt) {
+  const scrubTime = (evt.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 
 // Hook up event listeners
 playButton.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updatePlayButton);
 video.addEventListener("pause", updatePlayButton);
+video.addEventListener("timeupdate", updateProgressBar); // or 'progress' event, but 'progress' isn't automatic
 
 skipButtons.forEach(button => button.addEventListener("click", skip));
 
-volumeSlider.addEventListener("change", handleRangeUpdate);
-volumeSlider.addEventListener("mousemove", handleRangeUpdate);
-playrateSlider.addEventListener("change", handleRangeUpdate);
-playrateSlider.addEventListener("mousemove", handleRangeUpdate);
+let sliderMousedown = false;
+sliders.forEach(slider => {
+  slider.addEventListener("mousedown", () => sliderMousedown = true);
+  slider.addEventListener("mouseup", () => sliderMousedown = false);
+  slider.addEventListener("mouseout", () => sliderMousedown = false);
+  slider.addEventListener("change", handleRangeUpdate);
+  slider.addEventListener("mousemove", function (evt) {
+    sliderMousedown && handleRangeUpdate(evt);
+  });
+});
+
+let progressMousedown = false; // limits mousemove event to trigger scrub only if mouse is down
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (evt) => progressMousedown && scrub(evt));
+progress.addEventListener("mousedown", () => progressMousedown = true);
+progress.addEventListener("mouseup", () => progressMousedown = false);
+// progress.addEventListener("mouseout", () => progressMousedown = false);
+
+fullscreenButton.addEventListener("click", () => video.requestFullscreen());
